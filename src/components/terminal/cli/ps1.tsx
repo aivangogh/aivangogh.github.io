cc3fb508ae76a621411aa36027d3517da7c6eea4
Ivan Gemota <ivangemota23@gmail.com>
2025-02-20 20:38:20 +0800

    fixed inline issue in PS1 component

--- src/components/terminal/cli/ps1.tsx
+++ src/components/terminal/cli/ps1.tsx
@@ -4,20 +4,19 @@ export function PS1() {
 	const currentTheme = useThemeStore((state) => state.getTerminalColorScheme());
 
 	return (
-		<span className="font-medium flex flex-row">
-			<span className="" style={{ color: currentTheme?.yellow }}>
+		<span className="font-medium flex flex-row w-fit whitespace-nowrap">
+			<span style={{ color: currentTheme?.yellow }}>
 				ivan
 			</span>
-			<span className="" style={{ color: currentTheme?.foreground }}>
+			<span style={{ color: currentTheme?.foreground }}>
 				@
 			</span>
 			<span
-				className="whitespace-nowrap"
 				style={{ color: currentTheme?.green }}
 			>
 				portfolio
 			</span>
-			<span className="flex" style={{ color: currentTheme?.foreground }}>:$</span>
+			<span style={{ color: currentTheme?.foreground }}>:$</span>
 		</span>
 	);
 }
